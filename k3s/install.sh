b9a8a988e597cee7cbc9d56c180ff9cabb50bb00
Konstantin <git>
2024-09-15 09:54:55 +0200

    music player as k3s container

--- k3s/install.sh
+++ k3s/install.sh
@@ -1,30 +1,42 @@
-SECRET="mr_cluster_secret"
-IP=192.168.XX.XX
+SECRET=$0
+IP=$1
+URL=https://${IP}:6443
 echo https://${IP}:6443
 
-# docker exec -ti <container_id> reset-password
 
-sudo apt-get -y install linux-modules-extra-raspi 
+# https://docs.k3s.io/datastore/ha-embedded
+# https://docs.k3s.io/installation/uninstall
+# docker exec -ti <container_id> reset-password
+sudo apt-get update
+sudo apt-get -y install linux-modules-extra-raspi curl
+sudo apt-get -y install nfs-common
 
 # Master
-#curl -sfL https://get.k3s.io | sh -s server --cluster-init --token ${SECRET}
-curl -sfL https://get.k3s.io | K3S_TOKEN=${SECRET} sh -s - server --cluster-init
+#curl -sfL https://get.k3s.io | sh -s server --cluster-init --token=${SECRET}
+curl -sfL https://get.k3s.io | K3S_TOKEN=${SECRET} sh -s - server --cluster-init --disable=servicelb --no-deploy=kubernetes-dashboard
+
+# Disable ingress on all server
+#--disable traefik
 
 # Master Slave
-#curl -sfL https://get.k3s.io | K3S_TOKEN=${SECRET} sh -s - server --server https://${IP}:6443
+curl -sfL https://get.k3s.io | K3S_TOKEN=${SECRET} sh -s - server --server https://${IP}:6443 --disable=servicelb
 
 # Slave
 sudo curl -sfL https://get.k3s.io | K3S_TOKEN=${SECRET} K3S_URL=https://${IP}:6443 sh -
 
-# K3S_NODE_NAME="redNode1" sh -
+
+# K3S_NODE_NAME="mr00" sh -
+sudo curl -sfL https://get.k3s.io | K3S_TOKEN="mr_cluster_infra_20" sh -s - server --cluster-init --disable=servicelb
+sudo curl -sfL https://get.k3s.io | K3S_TOKEN="mr_cluster_infra_20" sh -s - server --server https://192.168.00.200:6443 --disable=servicelb
+sudo curl -sfL https://get.k3s.io | K3S_TOKEN="mr_cluster_infra_20" K3S_URL=https://192.168.00.200:6443 sh -s - agent
+
+## Comon
+#sed -e '/server \\/,$d' -e 's@ExecStart=.*@ExecStart=/usr/local/bin/k3s server@' -i /etc/systemd/system/k3s.service --disable=servicelb
+#systemctl daemon-reload
 
 
-# Comon
-sed -e '/server \\/,$d' -e 's@ExecStart=.*@ExecStart=/usr/local/bin/k3s server@' -i /etc/systemd/system/k3s.service
-systemctl daemon-reload
 
 # Check https://docs.k3s.io/advanced
 # https://kubernetes.io/docs/reference/kubectl/cheatsheet/
-sudo kubectl get nodes
-sudo kubectl get pods --all-namespaces
+
 
