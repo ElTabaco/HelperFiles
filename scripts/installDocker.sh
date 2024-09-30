543f4cc39eebbd28d6aa73efd4fbf38b0eb55ea7
Konstantin <git>
2024-09-30 10:52:18 +0200

    Remove ported music player

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -6,11 +6,10 @@ sudo apt-get update
 
 # https://github-wiki-see.page/m/spaceshiptrip/raspberrypi/wiki/Setup-Docker
 sudo curl -fsSL https://get.docker.com |bash
+echo "Add user to docker group"
 sudo usermod -aG docker mr
 #sudo apt purge --autoremove -y docker-compose
 
-# Add user to docker group
-sudo usermod -aG docker mr
 # sudo gpasswd -a $USER docker
 # https://github.com/slyfox1186/script-repo/blob/main/Bash/Misc/docker-compose-multi-arch.sh
 
@@ -19,5 +18,8 @@ sudo usermod -aG docker mr
 # sudo docker system -y prune -a && sudo docker container prune -f && sudo docker volume prune && sudo docker image prune -f
 
 # Login
-# sudo docker exec -u root -it <container_id> /bin/bash
+# docker exec -u root -it <container_id> /bin/bash
+# docker run --rm -u root -it -p 8200:8200 -p 12345:12345 --entrypoint sh alpine:latest
+# docker run --rm --network host -it --entrypoint sh alpine:latest
+
 # Ports:   https://www.cyberciti.biz/faq/unix-linux-check-if-port-is-in-use-command/
