e17449ef3bb09224160edab1fa12a7dd273a7c46
Konstantin <git>
2025-01-22 22:17:03 +0100

    moved openhab

--- k3s/openhab/delete.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f mr-do-openhab-deployment.yml
-kubectl delete -f mr-do-openhab-services.yml
-kubectl delete -f mr-do-openhab-pvc.yml
-kubectl delete -f mr-do-openhab-pv.yml
-kubectl delete namespace mr-do-openhab
