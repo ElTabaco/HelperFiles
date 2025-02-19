8dd979324e51f656642f11591dc15cc42c7a9e98
Konstantin <70294424+ElTabaco@users.noreply.github.com>
2025-02-19 16:52:31 +0100

    Update installDocker.sh

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-echo "Full Upgrade"
-sudo apt-get update
-# sudo apt-get -y install & sudo apt-get -y full-upgrade
-
 # https://github-wiki-see.page/m/spaceshiptrip/raspberrypi/wiki/Setup-Docker
+# https://github.com/slyfox1186/script-repo/blob/main/Bash/Misc/docker-compose-multi-arch.sh
+echo "Install docker and docker compose"
+sudo apt-get update
+
 sudo curl -fsSL https://get.docker.com |bash
 echo "Add user to docker group"
 sudo usermod -aG docker mr
-#sudo apt purge --autoremove -y docker-compose
 
+
+# Needful commands
 # sudo gpasswd -a $USER docker
-# https://github.com/slyfox1186/script-repo/blob/main/Bash/Misc/docker-compose-multi-arch.sh
-
 #sudo docker-compose up --force-recreate --build -d
 
 # sudo docker system -y prune -a && sudo docker container prune -f && sudo docker volume prune && sudo docker image prune -f
