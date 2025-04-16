bad3ca89c5702a37c1b7034b220e916c2285b443
Konstantin <70294424+ElTabaco@users.noreply.github.com>
2025-04-16 16:36:17 +0200

    Update updateUbuntu.sh

--- scripts/updateUbuntu.sh
+++ scripts/updateUbuntu.sh
@@ -10,9 +10,30 @@ dpkg --list | grep -i -E --color 'linux-image|linux-kernel' | grep '^ii'
 dpkg --list | egrep -i --color 'linux-image|linux-headers' | grep '^ii' | wc -l
 sudo apt-get --purge remove $(dpkg --list | egrep -i 'linux-image|linux-headers' | awk '/ii/{ print $2}' | egrep -v "$i")
 
-sudo apt -y -f autoremove --purge
+echo ">>> Cleaning apt cache..."
+sudo apt -y -f autoremove  --purge
+sudo apt -y -f clean
+sudo apt -y -f autoclean
 sudo apt -y -f autoremove 
-sudo apt autoclean
+
+echo ">>> Removing backup and leftover config files..."
+sudo find /etc -name "*~" -type f -delete
+sudo find /etc -name "*.bak" -type f -delete
+sudo find /etc -name "*.dpkg-old" -type f -delete
+sudo find /etc -name "*.dpkg-dist" -type f -delete
+
+echo ">>> Truncating all log files..."
+sudo find /var/log -type f -exec truncate -s 0 {} \;
+
+echo ">>> Deleting user trash and backup files..."
+rm -rf -v ~/.local/share/Trash/*
+find ~ -type f -name "*~" -delete
+find ~ -type f -name "*.bak" -delete
+
+echo ">>> Clean-up done!"
+
+
+echo ">>> Removing cache and logs..."
 #sudo apt-get --purge remove $(dpkg --list | egrep -i 'linux-image|linux-headers' | awk '/ii/{ print $2}' | egrep -v "$i")
 sudo rm -r -v /boot/firmware/*.bak
 sudo rm -r -v /boot/firmware/overlays/*.bak
@@ -20,9 +41,13 @@ sudo rm -r -v /boot/*.old
 sudo rm -fR -v /var/lib/apt/lists /tmp/* /var/tmp/*
 sudo rm -rf -v /var/lib/snapd /snap /var/snap /var/log/* /var/cache/* ~/.cache/*
 
-sudo journalctl --vacuum-size=5M
 
-echo "Configuration"
+echo ">>> Removing old journal logs (if using systemd-journald)..."
+sudo journalctl --vacuum-size=1M
+
+echo ">>> Clean-up done!"
+
+# echo "Configuration"
 # sudo timedatectl set-timezone Europe/Berlin
 # sudolocaledef -i en_US -f UTF-8 en_US.UTF-8
 
